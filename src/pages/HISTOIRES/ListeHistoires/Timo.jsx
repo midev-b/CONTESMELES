599f17a36e2dd655494e1ac649c558aa2818c8a0
Mina <belkmi22@gmail.com>
2024-03-08 14:01:24 +0100

    temp

--- src/pages/HISTOIRES/ListeHistoires/Timo.jsx
+++ src/pages/HISTOIRES/ListeHistoires/Timo.jsx
@@ -13,73 +13,50 @@ export function Timo() {
   const handleNext = () => {
     setCurrentIndex((prev) => prev + 1);
     setIsNextClicked(true);
+    console.log(currentIndex);
   };
 
   const handlePrevious = () => {
     setCurrentIndex((prev) => prev - 1);
     setIsNextClicked(false);
+    console.log(currentIndex);
   };
 
   const flipPage = (nextOrPrev) => {
     console.log(currentIndex);
     if (nextOrPrev === "next") {
-      return handleNext();
+      handleNext();
     } else {
-      return handlePrevious();
+      handlePrevious();
     }
   };
 
   return (
     <div className={styles.container}>
-      {text.map((el, i) => {
-        if (el.imageLeft === undefined || el.imageRight === undefined) {
-          return (
-            <div
-              className={`${styles.imageText}  ${
-                isNextClicked && (i === currentIndex || i < currentIndex)
-                  ? styles.flip
-                  : styles.notFlip
-              }`}
-              key={i}
-            >
-              <div
-                className={`${styles.page1} ${styles.backgrd}
-                `}
-              >
-                <p dangerouslySetInnerHTML={stringToHTML(el.text1)}></p>
-                <button onClick={() => flipPage("next")}>Next</button>
-              </div>
-              <div className={`${styles.page2} ${styles.backgrd}`}>
-                <p dangerouslySetInnerHTML={stringToHTML(el.text2)}></p>
-                <button onClick={() => flipPage("previous")}>Previous</button>
-              </div>
-            </div>
-          );
-        } else {
-          return (
-            <div
-              className={`${styles.imageText} ${
-                isNextClicked && (i === currentIndex || i < currentIndex)
-                  ? styles.flip
-                  : styles.notFlip
-              }`}
-              key={i}
-            >
-              <div className={`${styles.page1}`}>
-                <p dangerouslySetInnerHTML={stringToHTML(el.text1)}></p>
-                <img src={el.imageLeft} alt="Left Image" />
-                <button onClick={() => flipPage("next")}>Next</button>
-              </div>
+      {text.map((el, i) => (
+        <div
+          className={`${styles.imageText} ${
+            isNextClicked && (i === currentIndex || i < currentIndex)
+              ? styles.flip
+              : styles.notFlip
+          }`}
+          key={i}
+        >
+          <div className={`${styles.page1} ${!el.image1 && styles.backgrd}`}>
+            <p dangerouslySetInnerHTML={stringToHTML(el.text1)}></p>
+            {el.image1 && <img src={el.image1} alt="Left Image" />}
+            <button onClick={() => flipPage("next")}>Next</button>
+          </div>
 
-              <div className={`${styles.page2}`}>
-                <p dangerouslySetInnerHTML={stringToHTML(el.text2)}></p>
-                <img src={el.imageRight} alt="Right Image" />
-                <button onClick={() => flipPage("previous")}>Previous</button>
-              </div>
-            </div>
-          );
-        }
-      })}
+          <div className={`${styles.page2} ${!el.image2 && styles.backgrd}`}>
+            {el.text2 && (
+              <p dangerouslySetInnerHTML={stringToHTML(el.text2)}></p>
+            )}
+            {el.image2 && <img src={el.image2} alt="Right Image" />}
+            <button onClick={() => flipPage("previous")}>Previous</button>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
--- src/pages/HISTOIRES/ListeHistoires/timo.js
+++ src/pages/HISTOIRES/ListeHistoires/timo.js
@@ -17,132 +17,144 @@ import image16 from "./timoImages/image16.jpg";
 
 const text = [
   {
-    text1: `Timo est un petit hérisson , il vit avec ses parents dans
-      une forêt très lointaine.... `,
-    text2: ` Il a de petites pattes,
-    de jolies oreilles arrondies et des... piquants qui deviennent de plus en plus grands.`,
-    imageLeft: image1,
-    imageRight: image2,
+    text1: `Timo, le hérisson qui n'aimait pas ses piquants`,
+
+    text2: `Timo est un petit hérisson , il vit avec ses parents dans
+    une forêt très lointaine.... `,
+    image2: image1,
   },
+
   {
-    text1: `Un beau jour, il décide d'aller voir Boby le chien : <br>
+    text1: ` Il a de petites pattes,
+    de jolies oreilles arrondies et des... piquants qui deviennent de plus en plus grands.`,
+
+    text2: `Un beau jour, il décide d'aller voir Boby le chien : <br>
    <span> "Bonjour Boby, je peux jouer avec toi ?"</span> <br>
 Boby lui passe son ballon rouge.`,
-    text2: `Timo l'attrape et soudain il explose!<br>
+    image1: image2,
+    image2: image3,
+  },
+
+  {
+    text1: `Timo l'attrape et soudain il explose!<br>
  <span>"Va-t-en d'ici Timo",</span> dit furieusement le chien.
 Timo, triste, s'en va....`,
 
-    imageLeft: image3,
-    imageRight: image4,
-  },
-  {
-    text1: `Cette fois-ci , il croise Sissi la chatte sur son beau vélo rose<br>
+    text2: `Cette fois-ci , il croise Sissi la chatte sur son beau vélo rose<br>
   <span> "Bonjour Sissi,  veux-tu savoir comment je fais pour pédaler jusqu'au sommet de la montagne ?".
    </span> dit Timo.<br>
   <span> "Je suis curieuse de  voir cela,  tiens!".</span>`,
-    text2: `
+    image1: image4,
+    image2: image5,
+  },
+  {
+    text1: `
    Les roues du vélo éclatent !<br>
 Sissi se met en colère et dit :<span> " Tu es méchant Timo, tu as gâché mon vélo" </span> .`,
 
-    imageLeft: image5,
-    imageRight: image6,
-  },
-  {
-    text1: `Le hérisson ne comprend pas  ce qui se passe et se 
+    text2: `Le hérisson ne comprend pas  ce qui se passe et se 
   rend à la plage, histoire de se remonter le moral,  là où il rencontre Nomy la tortue.<br>
   Nomy est connue pour sa générosité et sa grande  gentillesse.
    Elle peut parfois être futée mais c'est rare.<br>
    <span>" Bonjour Timo, qu'est-ce qui ne va pas ?" </span> <br>
    Timo lui raconte tout ce qui  s'est passé avec lui .<br>
-   
-  
-  `,
-    text2: `
+   `,
+    image1: image6,
+    image2: image7,
+  },
+  {
+    text1: `
     <span> " Prends ma bouée et va te baigner,  ça te fera du bien " </span> a proposé Nomy.<br>
    À peine le hérisson frôle-t-il la bouée,  elle éclate! <br>
    <span> " Je crois savoir pourquoi tu ne fais que des dégâts depuis le matin , 
    c'est certainement à cause de tes pics " </span> réplique Nomy,  toute fière de sa trouvaille ! `,
-    imageLeft: image7,
-    imageRight: image8,
-  },
-  {
-    text1: ` <span>" Ne t'inquiète pas , je vais en parler à mon coiffeur attitré pour voir
+
+    text2: ` <span>" Ne t'inquiète pas , je vais en parler à mon coiffeur attitré pour voir
      s'il peut faire quelque chose pour toi " </span>.<br>
   Les deux amis vont chez Dino, le meilleur coiffeur de la forêt. 
   
   `,
-    text2: ` Une fois arrivés,  Dino sort ses petits ciseaux..
+    image1: image8,
+    image2: image9,
+  },
+  {
+    text1: ` Une fois arrivés,  Dino sort ses petits ciseaux..
   Une épine, deux épines et.... crac :<br>
    Les ciseaux se cassent.  `,
 
-    imageLeft: image9,
-    imageRight: image10,
-  },
-  {
-    text1: `Timo agacé par la malédiction qui semble s'être abattue sur lui ,
+    text2: `Timo agacé par la malédiction qui semble s'être abattue sur lui ,
      rentre à la maison en pleurant: <br>
   <span> " Je ne veux plus être un hérisson,  je ne veux plus de pics sur mon dos"</span><br> .
    <span> " Ne t'inquiète pas Timo, il viendra un jour où tes amis comprendront que tu es vraiment
    quelqu'un de spécial " <span>  dit la maman.
    `,
+    image1: image10,
+  },
 
-    text2: `Timo n'est guère réconforté par les  propos de sa maman et prend son sac à dos et quitte la maison  dans l'espoir de trouver une solution.   
+  {
+    text1: `Timo n'est guère réconforté par les  propos de sa maman et prend son sac à dos et quitte la maison  dans l'espoir de trouver une solution.   
    Sur la route , il demande à Nomy de l'accompagner.<br> 
         <span>  " Pas question de te laisser tomber "</span> répond Nomy.<br>
    Les deux amis empruntent le chemin qui mène à la montagne et marchent jusqu'à ce qu'ils aient faim.
   Pendant qu'ils savourent tranquillement leur repas , Nomy observe un moineau voler au-dessus des arbres : 
   `,
-  },
-  {
-    text1: `"<span>Et si tu apprenais à voler comme un moineau,  
+
+    text2: `"<span>Et si tu apprenais à voler comme un moineau,  
     ça ne doit pas être si dur que ça"</span> <br>
    <span> " Mais je n'ai pas d'ailes , je ne pourrai jamais voler "</span> répond Timo.<br>
   " L'habit ne fait pas le moineau, comme on dit , je suis sûre qu'avec un peu d'entraînement,
          
-    tu arriveras à voler aussi haut que les moineaux".</span>  dit la tortue .      `,
+    tu arriveras à voler aussi haut que les moineaux".</span>  dit la tortue . `,
+    image2: image11,
+  },
 
-    text2: `
+  {
+    text1: `
   Sous les encouragements de son amie , Timo se jette du haut d'une falaise et chute dans une  flaque d'eau . 
   Nomy ne se décourage pas , elle est déterminée à continuer l'aventure jusqu'au bout ; 
   `,
-    imageLeft: image11,
-    imageRight: image12,
-  },
-  {
-    text1: ` elle sort son GPS pour se diriger vers la rivière ,
+
+    text2: ` elle sort son GPS pour se diriger vers la rivière ,
      histoire d'essayer autre chose que les oiseaux .. <br> 
    <span>-Tu es très à la page Nomy dis donc ?!</span> dit Timo en voyant son GPS" <br>
     <span> -Oui , il faut toujours avoir un GPS avec soi , on est quand même au 15ème siècle </span><br>
   
     <span>-21 ème siècle Nomy , 21ème! </span> répond Timo. 
+    
   `,
-    text2: `Une fois arrivés , ils observent un petit poisson qui zigzague dans tous les sens . <br>
+    image1: image12,
+  },
+  {
+    text1: `Une fois arrivés , ils observent un petit poisson qui zigzague dans tous les sens . <br>
   <span> -Et si tu apprenais à nager comme un poisson Timo ?<span>  Suggère Nomy, <br>
    <span> -En plus tu n'auras même pas besoin d'ailes pour le faire.</span> Ajoute-elle<br>
    <span>-Certes, les poissons n'ont pas d'ailes mais ils ont des nageoires Nomy!<span>  
  `,
-  },
-  {
-    text1: `Soudain, les deux amis entendent une voix venant de loin : C'est Boby le chien et Sissi la chatte qui crient au secours ! 
+
+    text2: `Soudain, les deux amis entendent une voix venant de loin : C'est Boby le chien et Sissi la chatte qui crient au secours ! 
  Ils décident de rebrousser chemin et de venir en aide à leurs amis ,
   Quand ils arrivent, ils  voient  un grand loup . 
 
  `,
-    text2: `Timo comprend que ses amis sont en danger , il prend son courage
+
+    image2: image13,
+  },
+  {
+    text1: `Timo comprend que ses amis sont en danger , il prend son courage
  à quatre pattes , il se met en boule et se jette en direction du loup . 
 Le loup terrifié par les grosses épines du hérisson , prend la fuite et 
  quitte à tout jamais la forêt .`,
-    imageLeft: image13,
-    imageRight: image14,
-  },
-  {
-    text1: `Boby, Sissy et tous les animaux de la forêt le remercient de leur avoir 
+
+    text2: `Boby, Sissy et tous les animaux de la forêt le remercient de leur avoir 
     sauvé la vie et organisent une grande fête à l'honneur du nouveau héros . 
  `,
-    text2: ` 
+    image1: image14,
+    image2: image15,
+  },
+  {
+    text1: ` 
   Timo qui fait la fierté de tous les hérissons , est désormais très fier de  ce qu'il est`,
-    imageLeft: image15,
-    imageRight: image16,
+    image1: image16,
   },
 ];
 
